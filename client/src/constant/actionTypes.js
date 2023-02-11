e88cbf1257bc378b91533ef79fe2636b5f5a7438
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2023-02-11 04:43:37 +0100

    WIP: Added Live Server Url

--- client/src/constant/actionTypes.js
+++ client/src/constant/actionTypes.js
@@ -1,3 +1,3 @@
 export const DEV_BASE_URL = "http://localhost:5000"
-export const LIVE_BASE_URL = "http://localhost:5000";
+export const LIVE_BASE_URL = "https://decentral-frontend-task.vercel.app";
 export const NODE_ENV = import.meta.env.VITE_NODE_ENV;
