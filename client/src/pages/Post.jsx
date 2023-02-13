e93955b4067c6b9430acdae57f97a9fa2a9a377f
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2023-02-13 10:12:15 +0100

    WIP: Fixed All Major Bugs

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 import { Form } from "../components";
 
 const Post = () => {
+  const navigate = useNavigate();
+
+  const CheckUser = () => {
+    const user = JSON.parse(localStorage.getItem("profile"));
+    if (!user) {
+      navigate("/auth");
+    }
+  };
+
+  useEffect(() => {
+    CheckUser();
+  });
   return (
     <div>
       <Form />
